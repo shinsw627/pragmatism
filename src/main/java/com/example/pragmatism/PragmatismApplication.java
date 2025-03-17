f02f91543b37b10c7bebfa69cca76d207aa7e204
shinsw627 <shinsw627@naver.com>
2025-03-17 13:47:03 +0900

    init project

---
+++ src/main/java/com/example/pragmatism/PragmatismApplication.java
@@ -0,0 +1,13 @@
+package com.example.pragmatism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PragmatismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PragmatismApplication.class, args);
+	}
+
+}
